bb6c32adc57f59a120fb5c0a535aa5bb5c092682
Fauzii <113253689+FauziSlebew@users.noreply.github.com>
2025-08-04 04:34:33 +0800

    Update Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -2,7 +2,7 @@ const Senin = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Dasar Desain Grafis</div>
+					<div className="w-[50%] text-base  font-medium">DPTM</div>
 					<div className="flex justify-center items-center text-sm">08.00-09.20</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
@@ -10,11 +10,11 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm">09.20-10.00</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">Dasar Desain Grafis</div>
+					<div className="w-[50%] text-base  font-medium">SDY</div>
 					<div className="flex justify-center items-center text-sm">10.00-11.00</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">TJKT Cloud - NAS</div>
+					<div className="w-[50%] text-base  font-medium">MTK</div>
 					<div className="flex justify-center items-center text-sm">11.00-12.20</div>
 				</div>
 
@@ -23,7 +23,7 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm">12.20-13.20</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">Bimbingan Konseling</div>
+					<div className="w-[50%] text-base font-medium">P.PANCASILA</div>
 					<div className="flex justify-center items-center text-sm">13.20-14.40</div>
 				</div>
         </>
